d00b5d7d0ff0782c14a6c494cdf62263350adbdb
Ellen <ddhkxxhxx@hotmail.com>
2024-07-13 10:41:13 +0800

    reducer refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,16 @@
 import { useReducer, useState } from 'react'
-import { CartContext } from './store';
+import { CartContext, cartReducer, cartInit } from './store';
 
 import Navbar from './components/Navbar'
 import Products from './components/Products';
 import Cart from './components/Cart';
 
 
-
-
-
 function App() {
-  const cartReducer = useReducer((state, action) => {
-    const cartList = [...state.cartList]
-    const index = cartList.findIndex((item) => item.id === action.payload.id)
-    console.log(action)
-    switch(action.type) {
-      case 'ADD_TO_CART' :
-        if(index === -1){
-          cartList.push(action.payload)
-        } else {
-          cartList[index].quantity += action.payload.quantity
-        }
-
-        return {
-          ...state,
-          cartList,
-          total: calculateTotalPrice(cartList)
-        }
-      case 'CHANGE_CART_QUANTITY' :
-        cartList[index].quantity = action.payload.quantity
-        return {
-          ...state,
-          cartList,
-          total: calculateTotalPrice(cartList)
-        }
-      case 'REMOVE_CART_ITEM' :
-        cartList.splice(index, 1)
-        return {
-          ...state,
-          cartList,
-          total: calculateTotalPrice(cartList)
-        }
-      default:
-        return state
-    }
-  }, {
-    cartList: []
-  })
+  const reducer = useReducer(cartReducer, cartInit)
 
   return (
-    <CartContext.Provider value={cartReducer}>
+    <CartContext.Provider value={reducer}>
       <Navbar />
       <div className="container mt-4">
         <div className="row">
@@ -66,9 +27,5 @@ function App() {
   )
 }
 
-function calculateTotalPrice(cartList) {
-  return cartList.map((item) => item.quantity * item.price)
-    .reduce((a, b) => a + b, 0);
-}
 
 export default App
--- src/store.jsx
+++ src/store.jsx
@@ -1,3 +1,47 @@
 import { createContext } from "react";
 
+function calculateTotalPrice(cartList) {
+  return cartList.map((item) => item.quantity * item.price)
+    .reduce((a, b) => a + b, 0);
+}
+
+
 export const CartContext = createContext()
+
+export const cartReducer = (state, action) => {
+  const cartList = [...state.cartList]
+  const index = cartList.findIndex((item) => item.id === action.payload.id)
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      if (index === -1) {
+        cartList.push(action.payload)
+      } else {
+        cartList[index].quantity += action.payload.quantity
+      }
+      return {
+        ...state,
+        cartList,
+        total: calculateTotalPrice(cartList)
+      }
+    case 'CHANGE_CART_QUANTITY':
+      cartList[index].quantity = action.payload.quantity
+      return {
+        ...state,
+        cartList,
+        total: calculateTotalPrice(cartList)
+      }
+    case 'REMOVE_CART_ITEM':
+      cartList.splice(index, 1)
+      return {
+        ...state,
+        cartList,
+        total: calculateTotalPrice(cartList)
+      }
+    default:
+      return state
+  }
+}
+
+export const cartInit = {
+  cartList: []
+}
