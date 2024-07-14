8f197cf73b960aa096c81900142fefea1c3c7d8b
Ellen <ddhkxxhxx@hotmail.com>
2024-07-14 11:56:57 +0800

    add function of select quantity

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,13 @@
 
 import { CartContext } from "../store"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 
 import productsData from "../assets/productsData"
 
 
 function Products() {
   const [state, dispatch] = useContext(CartContext)
+  const [qty, setQty]=useState(1)
 
   return (<div className="row row-cols-3 g-3">
     {
@@ -18,14 +19,37 @@ function Products() {
               <h6 className="card-title">{product.title}
                 <span className="float-end">NT$ {product.price}</span>
               </h6>
+              <select name='' id='' className="qty"onChange={((e) => {
+                e.preventDefault()
+                const quantity = parseInt(e.target.value) 
+                dispatch({
+                  type: 'CHANGE_PRODUCT_QUANTITY',
+                  payload: {
+                    ...product,
+                    quantity
+                  }
+                })
+              })}>
+                {
+                  [...Array(20)].map((_,i) => {
+                    return( <option value={i+1} key={i}>{i+1}</option>
+                    )
+                  })
+                }
+              </select>
 
-              <button type="button" className="btn btn-outline-primary w-100" onClick={()=>dispatch({
+              <button type="button" className="btn btn-outline-primary w-100" 
+              onClick={(e)=> {
+                const quantity = parseInt(e.target.previousSibling)
+                console.log(quantity)
+                e.target.previousSibling.value = 1
+                dispatch({
                 type: 'ADD_TO_CART',
                 payload: {
                   ...product,
-                  quantity: 1
+                  quantity: quantity? quantity: 1
                 }
-              })}>加入購物車</button>
+              })}}>加入購物車</button>
             </div>
           </div>
         </div>
